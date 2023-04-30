b7a5a79681be9d64a9b8c011f3a61a4cf76295ec
pvwnthem <pvwn.them@gmail.com>
2023-04-29 21:19:35 -0400

    add placeholder navbar and simple navbar styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { Navbar } from "@/components/navbar";
 import React from "react";
 
-export default function Home() {
+export default function Index() {
   return (
-   <></>
+   <>
+    <Navbar />
+   </>
   )
 }
