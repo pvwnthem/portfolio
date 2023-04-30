dd40e99ce8e7bf4429c0e6f7fe68b00c6ee5f688
pvwnthem <pvwn.them@gmail.com>
2023-04-29 21:52:49 -0400

    add scroll snapping

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import Home from "@/components/home";
 import { Navbar } from "@/components/navbar";
-import React from "react";
+import useScrollSnap from 'react-use-scroll-snap';
+import React, { useRef } from "react";
 
 export default function Index() {
+
+  const scrollRef = useRef(null);
+  useScrollSnap({ ref: scrollRef, duration: 25, delay: 1 });
+
   return (
-   <>
+   <div ref={scrollRef}>
     <Navbar />
     <Home />
     <Home />
-   </>
+   </div>
   )
 }
