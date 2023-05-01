d4e89eac97093485fbb4ab32358f7f769a88b54a
pvwnthem <pvwn.them@gmail.com>
2023-04-30 20:53:32 -0400

    change color

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -12,7 +12,7 @@ export default function About () {
               className="w-lg md:mx-0 mx-auto shadow-lg rounded-xl"
             />
             <div className="flex-col md:p-4">
-              <h3 className=" uppercase mt-4 text-green-600 text-xl font-bold">about me</h3>
+              <h3 className=" uppercase mt-4 text-green-500 text-xl font-bold">about me</h3>
               <h1 className="mt-4 font-bold text-gray-700 text-2xl">A dedicated Full-Stack Developer based in the USA 📌</h1>
               <p className="mt-4 text-md text-gray-600 font-semibold">
               Hi im 2x1, a 13-year-old full stack developer with a passion for backend development. With expertise in TypeScript, JavaScript, MongoDB, React, Tailwind, and other programming languages and tools, I am skilled at building robust and scalable web applications. As an avid learner, i am always exploring new technologies and staying up to date with the latest industry trends. Despite my age, I am always a driven and dedicated developer who is committed to honing my skills and creating innovative solutions to complex problems.
