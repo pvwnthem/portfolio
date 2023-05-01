741ca751fbfa6ace860b7ff0c96cf47b666248b6
pvwnthem <pvwn.them@gmail.com>
2023-05-01 11:03:25 -0400

    favicon and title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <title>2x1.dev</title>
       <Head />
       <body>
         <Main />
