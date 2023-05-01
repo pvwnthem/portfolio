b56b984873652d64684298d8738dbf8ea5e3b064
pvwnthem <pvwn.them@gmail.com>
2023-04-30 23:45:15 -0400

    mobile friendly

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,26 +1,50 @@
-/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
-export default function About () {
+export default function About() {
   return (
-    <>
-     <div id="about" className="w-full h-screen py-16 bg-white md:flex flex-col items-center justify-center">
-        <div className="md:w-1/2 w-full md:space-x-14 space-x-2 md:px-0 px-2 border md:flex">
-            <img
-              src="https://via.placeholder.com/400x400"
-              alt="Placeholder image"
-              className="w-lg md:mx-0 mx-auto shadow-lg rounded-xl"
-            />
-            <div className="flex-col md:p-4">
-              <h3 className=" uppercase mt-4 text-green-500 text-xl font-bold">about me</h3>
-              <h1 className="mt-4 font-bold text-gray-700 text-2xl">A dedicated Full-Stack Developer based in the USA 📌</h1>
-              <p className="mt-4 text-md text-gray-600 font-semibold">
-              Hi im 2x1, a 13-year-old full stack developer with a passion for backend development. With expertise in TypeScript, JavaScript, MongoDB, React, Tailwind, and other programming languages and tools, I am skilled at building robust and scalable web applications. As an avid learner, i am always exploring new technologies and staying up to date with the latest industry trends. Despite my age, I am always a driven and dedicated developer who is committed to honing my skills and creating innovative solutions to complex problems.
-              </p>
-            </div>
-            
+    <div id="about" className="py-6 md:py-16 bg-white">
+      <div className="md:flex md:flex-col md:items-center md:justify-center">
+        <div className="md:w-1/2 border md:flex md:space-x-4">
+          <img
+            src="https://via.placeholder.com/400x400"
+            alt="Placeholder image"
+            className="w-full md:w-auto mx-auto md:mx-0 shadow-lg rounded-xl"
+          />
+          <div className="flex-1 md:p-4">
+            <h3 className="text-green-500 text-xl font-bold uppercase mt-4 md:mt-0">
+              About Me
+            </h3>
+            <h1 className="text-gray-700 text-2xl font-bold mt-4">
+              A dedicated Full-Stack Developer based in the USA 📌
+            </h1>
+            <p className="text-md text-gray-600 font-semibold mt-4">
+              Hi, I&apos;m 2x1, a 13-year-old full-stack developer with a passion for
+              backend development. With expertise in TypeScript, JavaScript,
+              MongoDB, React, Tailwind, and other programming languages and
+              tools, I am skilled at building robust and scalable web
+              applications. As an avid learner, I am always exploring new
+              technologies and staying up to date with the latest industry
+              trends. Despite my age, I am always a driven and dedicated
+              developer who is committed to honing my skills and creating
+              innovative solutions to complex problems.
+            </p>
+          </div>
         </div>
-     </div>
-    </>
+      </div>
+      <style jsx>{`
+        #about {
+          min-height: calc(100vh - 64px);
+        }
+
+        @media screen and (max-width: 640px) {
+          h1 {
+            font-size: 1.5rem;
+          }
+          p {
+            font-size: 1rem;
+          }
+        }
+      `}</style>
+    </div>
   );
 }
