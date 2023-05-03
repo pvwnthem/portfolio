6089c2c8050d0a1351e9f5d919ff6a9a87ff12db
pvwnthem <pvwn.them@gmail.com>
2023-05-03 15:23:46 -0400

    use random svg instead of image

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -5,11 +5,40 @@ export default function About() {
     <div id="about" className="py-6 md:py-16 md:h-screen flex bg-white">
       <div className="md:flex md:flex-col md:items-center md:justify-center">
         <div className="md:w-1/2  md:flex md:space-x-4">
-          <img
-            src="https://via.placeholder.com/400x400"
-            alt="Placeholder image"
-            className="w-full md:w-auto mx-auto md:mx-0 shadow-lg rounded-xl"
-          />
+        <svg
+                  className="w-1/2 text-green-600 md:mx-0 mx-auto"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M12 1L2 7V17L12 23L22 17V7L12 1Z"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M12 22V11"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M7 13H17"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M7 17H17"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
           <div className="flex-1 md:p-4">
             <h3 className="text-green-500 md:px-0 px-2 text-xl font-bold uppercase mt-4 md:mt-0">
               About Me
