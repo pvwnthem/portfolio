0c824abef0c10df519624d2512e8b86d589a7ec6
pvwnthem <pvwn.them@gmail.com>
2023-05-03 23:26:53 -0400

    remove snap scrolling (annoying)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,13 @@ import Home from "@/components/home";
 import About from "@/components/about";
 import Projects from "@/components/projects";
 import { Navbar } from "@/components/navbar";
-import useScrollSnap from 'react-use-scroll-snap';
 import React, { useRef } from "react";
 import Contact from "@/components/contact";
 
 export default function Index() {
-
-  const scrollRef = useRef(null);
-  useScrollSnap({ ref: scrollRef, duration: 25, delay: 1 });
-
+  
   return (
-   <div ref={scrollRef}>
+   <div>
     <Navbar />
     <Home />
     <About />
