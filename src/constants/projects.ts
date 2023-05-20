eaca75d58a5b82d7171f3fb3d1976a7c1444aeef
pvwn <114266525+pvwnthem@users.noreply.github.com>
2023-05-20 00:06:58 -0400

    update blockchain project tags

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -12,7 +12,7 @@ export const projects: project[] = [
     {
         title: "Proof of Work Blockchain",
         description: "I created a proof of work blockchain using typescript and crypto-js. This project taught me a lot about how blockchains work and how people interact with them. This project consists of a simple, object-oriented implementation of a proof of work blockchain.",
-        tags: ["Typescript", "Cryptography", "Blockchain"],
+        tags: ["Typescript", "Blockchain"],
         sourcelink: "https://github.com/pvwnthem/pow-blockchain",
         imageLink: "https://101blockchains.com/wp-content/uploads/2021/03/blockchain1a.png"
     },
