a6022ac78572e9911328783dbcd38870b7c5d00a
pvwn <114266525+pvwnthem@users.noreply.github.com>
2023-05-20 00:05:36 -0400

    fix typo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -69,7 +69,7 @@ export const Navbar = (): JSX.Element => {
                 className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-green-600 hover:bg-green-100 focus:outline-none focus:text-green-600 focus:bg-green-100">
                 Projects
               </button>
-              <button  onClick={() => {scroll('projects')}} className=" px-3 py-2 bg-green-600 hover:bg-green-700 rounded-md text-white">
+              <button  onClick={() => {scroll('contact')}} className=" px-3 py-2 bg-green-600 hover:bg-green-700 rounded-md text-white">
                 Contact
               </button>
             </div>
