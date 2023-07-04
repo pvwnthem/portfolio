2cd6fa4babad626391ffabd26149c790ce196cc8
pvwnthem <pvwn.them@gmail.com>
2023-07-04 01:19:07 +0000

    change wording, remove unused styling thing

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -47,7 +47,7 @@ export default function About() {
               A dedicated Full-Stack Developer based in the USA 📌
             </h1>
             <p className="text-md md:px-0 px-2 text-gray-600 font-semibold mt-4">
-              Hi, I&apos;m 2x1, a 13-year-old full-stack developer with a passion for
+              Hi, I&apos;m 2x1, a young full-stack developer with a passion for
               backend development. With expertise in TypeScript, JavaScript,
               MongoDB, React, Tailwind, and other programming languages and
               tools, I am skilled at building robust and scalable web
@@ -59,21 +59,7 @@ export default function About() {
             </p>
           </div>
         </div>
-      </div>
-      <style jsx>{`
-        #about {
-          min-height: calc(100vh - 64px);
-        }
-
-        @media screen and (max-width: 640px) {
-          h1 {
-            font-size: 1.5rem;
-          }
-          p {
-            font-size: 1rem;
-          }
-        }
-      `}</style>
+      </div>    
     </div>
   );
 }
